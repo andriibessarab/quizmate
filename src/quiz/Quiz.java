bb29ee54c0ad5f65bda20a7934cc2472fb2162f1
andriibessarab <andriibessarab@gmail.com>
2022-10-29 12:38:44 -0400

    Create quiz class

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -1,37 +1,11 @@
 package quiz;
 
-// Global imports
-import java.util.Scanner;
-
-// Local imports
-import fileManagement.*;
-import question.*;
+// Global Imports
+import java.util.ArrayList;
+import java.util.List;
 
 public class Quiz {
-    public static final Scanner scan = new Scanner(System.in);
+    public String quizName; // Quiz Name
+    public List<Question> questions = new ArrayList<Question>(); // List of answers
+}
 
-    //Create Quiz
-    public static void create() {
-        Question a = new Question();
-        a.question = "gfhdjfghjgfd";
-
-        Question b = new Question();
-        b.question = "gfhdjfghjgfd";
-
-        Question c = new Question();
-        c.question = "gfhdjfghjgfd";
-
-        System.out.print("Quiz Name: ");
-        String quizName = scan.nextLine();
-        fileManager.writeToFile(quizName, new Question[]{a, b, c}); // Create file
-
-
-
-
-    }
-
-    // Play Quiz
-    public static void play() {
-
-    }
-} 
