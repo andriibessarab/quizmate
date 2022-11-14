57e71c5e34e4811f4d08c4ee15dd0645ce3cb9c5
spderz12 <116905268+spderz12@users.noreply.github.com>
2022-11-13 22:01:17 -0500

    amended play code

--- src/game/Play.java
+++ src/game/Play.java
@@ -22,22 +22,21 @@ public class Play {
         List<String> allQuizes = fileManager.listAllObjects(); // list of all quizes. if none, return null
         if (allQuizes == null) {
             System.out.println("No quizzes made... Go create some!");
-           App callBack = new App(); //it says  the app class doesn't exist so im confused. I just want to call the main class
-            callBack.main();
+            
         }
         else {
             while (runCondition = true)
             {
                 System.out.println("Enter quiz you wish to play: ");
                 ans = scan.nextInt();
-                    if (ans != allQuizes.indexOf(i+1))
-                    {
-                        System.out.println("Not a valid quiz.");
-                    }
-                    else
-                    {
-                        Quiz quiz = fileManager.readObject(allQuizes.get(ans)); // get Quiz object for specific quiz // I figured this would work, but I doubt it - Jack
-                    }
+                if (ans != allQuizes.indexOf(i+1))
+                {
+                    System.out.println("Not a valid quiz.");
+                }
+                else
+                {
+                    Quiz quiz = fileManager.readObject(allQuizes.get(ans)); // get Quiz object for specific quiz // I figured this would work, but I doubt it - Jack
+                }
             }
 
 
@@ -47,3 +46,4 @@ public class Play {
         }
     }
 }
+
